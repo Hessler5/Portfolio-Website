12b6fc60ad58dd58a12444850d7243af8f4e6343
Ethan Hessler <ethanhessler@gmail.com>
2024-02-21 02:45:54 -0500

    fixed tailwind

--- portfolio-website/src/pages/error_page.jsx
+++ portfolio-website/src/pages/error_page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ErrorPage() {
     return(
         <div>
-            <h1>test</h1>
+            <h1>error</h1>
         </div>
     )
 }
--- portfolio-website/src/pages/home.jsx
+++ portfolio-website/src/pages/home.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function Home() {
     return(
         <div>
-            <h1>test</h1>
+            <h1 className = "text-gray-500">Welcome</h1>
+            <img className = "w-0.5" src="./src/assets/IMG_1567.JPG"/>
         </div>
     )
 }
